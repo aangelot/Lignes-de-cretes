5e446185ce9225df8ebc1a4ba8a05fce4f9865b6
aangelot <anthony.angelot@gmail.com>
2025-05-01 17:06:11 +0200

    Add map on welcome screen

--- hello/views.py
+++ hello/views.py
@@ -1,16 +1,10 @@
 import re
+import json
 from django.shortcuts import render
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 from .forms import HikeForm
 from .services.routing import compute_hike_path
-
-# Replace the existing home function with the one below
-def home(request):
-    return render(request, "hello/home.html")
-
-def contact(request):
-    return render(request, "hello/contact.html")
 
 
 def hello_there(request, name):
@@ -23,20 +17,21 @@ def hello_there(request, name):
         }
     )
 
-def plan_hike(request):
+def index(request):
     path = None
-    if request.method == 'POST':
-        form = HikeForm(request.POST)
-        if form.is_valid():
-            start_lat = float(request.POST.get("start_lat"))
-            start_lon = float(request.POST.get("start_lon"))
-            end_lat = float(request.POST.get("end_lat"))
-            end_lon = float(request.POST.get("end_lon"))
 
-            fmap = compute_hike_path((start_lat, start_lon), (end_lat, end_lon))
-            map_html = fmap._repr_html_()
-            return render(request, 'hello/plan_hike.html', {'form': form, "map_html": map_html})
-    else:
-        form = HikeForm()
-        return render(request, 'hello/plan_hike.html', {'form': form})
+    if request.method == 'POST':
+        start = request.POST.get('start')
+        end = request.POST.get('end')
+        if start and end:
+            try:
+                start_coords = tuple(map(float, start.split(',')))
+                end_coords = tuple(map(float, end.split(',')))
+                path = compute_hike_path(start_coords, end_coords)
+            except:
+                path = None
+    context = {
+        'path': json.dumps(path) if path else None,
+    }
+    return render(request, 'hello/index.html', context)
 
